f9d180ce7bd6b9298deebef5c1bf37876e491c40
wim van laer <van.laer.wim@gmail.com>
2019-07-23 13:54:19 +0200

    basic logic working

--- popup.js
+++ popup.js
@@ -1,10 +1,18 @@
 let increaseScore = document.getElementById('increaseScore');
-let apiAddress = "localhost:5000"
+let apiAddress = "http://localhost:5000"
 
 chrome.storage.sync.get('apiAddress', function(data) {
-    apiAddress = data.apiAddress
+    //apiAddress = data.apiAddress
 });
 
 increaseScore.onclick = function(element) {
     console.log("Squashed a fly");
+    let player = $("#player")[0].value
+    $.ajax({
+        type: 'PUT',
+        dataType: 'json',
+        url: `${apiAddress}/api/participants/${player}/increaseScore?amount=1`,
+        headers: {"X-HTTP-Method-Override": "PUT"},
+        data: ''
+    });
 };
