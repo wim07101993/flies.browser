01b32814ace1cea78f10c100e027cbf970ae00c6
wim van laer <van.laer.wim@gmail.com>
2019-07-23 14:35:54 +0200

    can save api address and increase score

--- options.js
+++ options.js
@@ -1,4 +1,11 @@
+chrome.storage.sync.get('apiAddress', function(data) {
+    $("#apiAddress")[0].value = data.apiAddress;
+});
+
 $("#save").click(function() {
-    let ipAddress = $("#ipAddress")[0].value;
-    console.log(`saved server ip address to ${ipAddress}`);
+    let ip = $("#apiAddress")[0].value;
+
+    chrome.storage.sync.set({apiAddress: ip}, function() {
+        console.log(`saved server ip address to ${ip}`);
+    });
 });
--- popup.js
+++ popup.js
@@ -2,16 +2,19 @@ let increaseScore = document.getElementById('increaseScore');
 let apiAddress = "http://localhost:5000"
 
 chrome.storage.sync.get('apiAddress', function(data) {
-    //apiAddress = data.apiAddress
+    console.log(data.apiAddress);
+    apiAddress = data.apiAddress;
 });
 
 increaseScore.onclick = function(element) {
     console.log("Squashed a fly");
-    let player = $("#player")[0].value
+    let player = $("#player")[0].value;
+    let url = `${apiAddress}/api/participants/${player}/increaseScore?amount=1`;
+    console.log(url);
     $.ajax({
         type: 'PUT',
         dataType: 'json',
-        url: `${apiAddress}/api/participants/${player}/increaseScore?amount=1`,
+        url: url,
         headers: {"X-HTTP-Method-Override": "PUT"},
         data: ''
     });
